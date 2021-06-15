972f68014b19f907462e2b7af56acfd1fee4f4c8
sasaplus1 <sasaplus1@gmail.com>
2021-06-15 21:29:18 +0900

    fix

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ var HistoryStack = /** @class */ (function () {
         return this._index >= 0;
     };
     HistoryStack.prototype.redo = function () {
-        if (this._index >= this._stack.length) {
-            throw new Error('cannot redo');
+        if (this._index >= this._stack.length - 1) {
+            throw new Error('Cannot redo');
         }
         return this._stack[++this._index];
     };
     HistoryStack.prototype.undo = function () {
         if (this._index < 0) {
-            throw new Error('cannot undo');
+            throw new Error('Cannot undo');
         }
         return this._stack[this._index--];
     };
--- index.ts
+++ index.ts
@@ -1,36 +1,31 @@
 export class HistoryStack<T> {
-  private _index: number;
-  private _stack: T[];
+  private _index = -1;
+  private _stack: T[] = [];
 
-  constructor() {
-    this._index = -1;
-    this._stack = [];
-  }
-
-  add(history: T) {
+  add(history: T): void {
     this._stack.splice(this._index + 1, Infinity, history);
     this._index = this._stack.length - 1;
   }
 
-  canRedo() {
+  canRedo(): boolean {
     return this._index < this._stack.length - 1;
   }
 
-  canUndo() {
+  canUndo(): boolean {
     return this._index >= 0;
   }
 
-  redo() {
-    if (this._index >= this._stack.length) {
-      throw new Error('cannot redo');
+  redo(): T {
+    if (this._index >= this._stack.length - 1) {
+      throw new Error('Cannot redo');
     }
 
     return this._stack[++this._index];
   }
 
-  undo() {
+  undo(): T {
     if (this._index < 0) {
-      throw new Error('cannot undo');
+      throw new Error('Cannot undo');
     }
 
     return this._stack[this._index--];
